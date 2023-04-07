4949690e9cb1a36122e7e2c46540bb8cce260ba4
garmin <zhangjiaming@ufactory.cc>
2023-04-07 18:23:55 +0800

    ctrl+s save file

--- template/client/src/pages/Ide/index.tsx
+++ template/client/src/pages/Ide/index.tsx
@@ -45,6 +45,17 @@ class Ide extends React.Component<{}, MyEditorState>  {
 				value={code} // 设置编辑器的初始内容
 				options={options} // 设置编辑器的选项
 				onChange={this.handleEditorChange} // 监听编辑器内容的变化
+				editorDidMount={(editor, monaco)=>{
+					editor.onKeyDown((event) => {
+						// 判断是否按下了 Ctrl + S
+						if (event.ctrlKey && event.keyCode === monaco.KeyCode.KeyS) {
+							event.preventDefault(); // 阻止默认事件，避免保存网页
+							console.log("Ctrl + S pressed"); // 在控制台输出消息
+							// 在这里执行保存操作或其他逻辑
+						}
+					});
+				}
+				}
 			/>
 		);
 	}
